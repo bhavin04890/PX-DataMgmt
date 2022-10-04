426f9e7b18023d8f4c506aa8e956d9dd307ea8d8
bhavin04890 <bshah@purestorage.com>
2022-10-04 16:19:28 -0400

    update to update-iops.sh

--- update-iops.sh
+++ update-iops.sh
@@ -4,6 +4,8 @@ PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0
 
 VolName=$(kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list | grep "47 GiB" | awk '{print $2}' )
 
+kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume inspect $VolName
+
 kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume update --max_iops 750,750 $VolName
 
 kubectl exec $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume inspect $VolName
